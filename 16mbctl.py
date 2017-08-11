74546ac3ddaf518a2a00fb09a3dc36b61861943f
julien <julien.bernard.iphone@gmail.com>
2017-08-11 21:43:14 +0200

    get all entrypoint

--- 16mbctl.py
+++ 16mbctl.py
@@ -37,20 +37,47 @@ def _handle_config():
 def _refresh_token(config):
     config['token'] = _authenticate(config['username'], config['url_root'])
     path = Path.home().joinpath('.16mb.json')
-    path.write_text()
+    path.write_text(json.dumps(config))
 
     return config
 
 
-def _make_post_request(url, data, headers, config, retry=False):
+def _pretify_json(content):
+    data = json.loads(content)
+    result = None
+    if isinstance(data, list):
+        cpt = 0
+        rows = list()
+        for row in data:
+            if cpt == 0:
+                rows.append('\t\t\t'.join(row.keys()))
+
+            rows.append('\t\t\t'.join([row[k] for k in row.keys()]))
+
+            cpt += 1
+
+        result = '\n'.join(rows)
+
+    return result
+
+
+def _make_request(verb, url, data, headers, config, retry=False):
     print("Requesting URL {}".format(url))
 
-    r = requests.post(url, data=json.dumps(data), headers=headers)
+    if verb == 'POST':
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        result = r.text
+    else:
+        if data:
+            r = requests.get(url, data=json.dumps(data), headers=headers)
+        else:
+            r = requests.get(url, headers=headers)
+        result = _pretify_json(r.text)
 
     if r.status_code == 401:
         if retry is False:
             config = _refresh_token(config)
-            _make_post_request(url, data, headers, config, retry=True)
+            _make_request(verb, url, data, headers, config, retry=True)
         else:
             print('Authentication failed')
             sys.exit(1)
@@ -67,29 +94,41 @@ def _make_post_request(url, data, headers, config, retry=False):
         print('Something goes wrong')
         sys.exit(1)
 
-    return r.text
+    return result
 
 
-def _handle_command(domain, subcommand, datafile, config):
-    with open(datafile, 'r') as f:
-        data = json.loads(f.read())
+def _handle_command(command, domain, subcommand, config, datafile=None):
+    data = None
+    if datafile:
+        with open(datafile, 'r') as f:
+            data = json.loads(f.read())
 
     headers = {'Authorization': config['token']}
-    url = ''.join([config['url_root'], '/api/v1/command/{}/{}'.format(domain, subcommand)])
 
-    result = _make_post_request(url, data, headers, config)
+    if command == 'cmd':
+        url = ''.join([config['url_root'], '/api/v1/command/{}/{}'.format(domain, subcommand)])
+        result = _make_request('POST', url, data, headers, config)
+    elif command == 'get':
+        url = ''.join([config['url_root'], '/api/v1/query/{}/{}'.format(domain, subcommand)])
+        result = _make_request('GET', url, data, headers, config)
+    else:
+        print('Unknown command type: {}'.format(command))
+        sys.exit(1)
 
     print(result)
 
 if __name__ == '__main__':
     config = _handle_config()
 
-    if len(sys.argv) != 4:
-        print('Wrong arguments. Usage <domain> <subcommand> <datafile>.json')
+    if len(sys.argv) < 1:
+        print('Wrong arguments. Usage <cmd|get> ...')
         sys.exit(1)
 
-    domain = sys.argv[1]
-    subcommand = sys.argv[2]
-    datafile = sys.argv[3]
+    command = sys.argv[1]
+    domain = sys.argv[2]
+    subcommand = sys.argv[3]
+    datafile = None
+    if len(sys.argv) > 4:
+        datafile = sys.argv[4]
 
-    _handle_command(domain, subcommand, datafile, config)
+    _handle_command(command, domain, subcommand, config, datafile)
