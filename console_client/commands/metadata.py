1a4c250cfff70479ecfc057e604179d586d877bc
Julien Bernard <julien.bernard.iphone@gmail.com>
2017-11-05 15:16:54 +0000

    rename command

--- console_client/commands/metadata.py
+++ console_client/commands/metadata.py
@@ -1,6 +1,6 @@
 from console_client.commands import PurePostCommand
 
 
-class AddTransformationCommand(PurePostCommand):
+class AddTransformation(PurePostCommand):
     name = 'add_transformation'
     url_suffix = '/api/v1/command/metadata/add_transformation'
